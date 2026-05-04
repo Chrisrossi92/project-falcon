6297991dd0c97d8ff86b95dc2aae0f7fffd45d1e
Chris Rossi <chrisrossi92@gmail.com>
2026-05-03 22:50:36 -0400

    Use panel variant for drawer Smart Actions

--- src/components/orders/view/QuickActionsDrawerPanel.jsx
+++ src/components/orders/view/QuickActionsDrawerPanel.jsx
@@ -60,7 +60,7 @@ export default function QuickActionsDrawerPanel({
 
   return (
     <Wrap>
-      <SmartActionsControl actions={smartActions} />
+      <SmartActionsControl actions={smartActions} variant="panel" />
     </Wrap>
   );
 }
